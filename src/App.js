cd8765692616b7d3bf0f8e1530e7c7cf9cbbf662
Faizan Siddiqui <siddiquimdfaizan82@gmail.com>
2021-02-16 16:48:03 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Login from "./components/login/login"
+import Singup from "./components/signup/signup"
+import Dashboard from "./components/dashboard/dashboard"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Router>
+        <nav>
+          <ul>
+            <li> 
+              <Link to="/">Login</Link>
+            </li>
+            <li> 
+              <Link to="/singup">Signup</Link>
+            </li>
+            <li> 
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+
+
+        <Switch>
+
+          <Route exact={true} path="/">
+            <Login/>
+          </Route>
+
+          
+          <Route path="/dashboard">
+            <Dashboard/>
+          </Route>
+
+          
+          <Route path="/singup">
+            <Singup/>
+          </Route>
+
+        </Switch>
+      </Router>
+
+    </div>  
   );
 }
-
 export default App;
---
+++ src/components/signup/signup.js
@@ -0,0 +1,13 @@
+
+
+
+
+function Singup() {
+
+
+    return (
+        <h1>Singup</h1>
+    )
+    
+}
+export default Singup;
